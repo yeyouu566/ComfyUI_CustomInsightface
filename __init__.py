918a053ad1e2af434c734a6ccd983cab57c59b18
yeyouu566 <kyy242734@gmail.com>
2025-05-02 20:50:03 +0900

    Update __init__.py

---
+++ __init__.py
@@ -0,0 +1,9 @@
+from .node_insightface import InsightFaceLoader
+
+NODE_CLASS_MAPPINGS = {
+    "InsightFaceLoader": InsightFaceLoader,
+}
+
+NODE_DISPLAY_NAME_MAPPINGS = {
+    "InsightFaceLoader": "InsightFace 모델 분석기",
+}

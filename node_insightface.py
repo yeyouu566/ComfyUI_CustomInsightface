c10ec1d91c375842e7231519265d4646cfebd368
yeyouu566 <kyy242734@gmail.com>
2025-05-07 14:11:31 +0900

    Update node_insightface.py

--- node_insightface.py
+++ node_insightface.py
@@ -2,10 +2,6 @@ from insightface.app import FaceAnalysis
 from PIL import Image
 import numpy as np
 
-TYPE_CLASSES = {
-    "INSIGHTFACE": list  # ComfyUI가 받아들일 수 있는 타입 지정
-}
-
 class InsightFaceLoader:
     def __init__(self):
         self.app = None
@@ -18,26 +14,25 @@ class InsightFaceLoader:
             }
         }
 
-    RETURN_TYPES = ("INSIGHTFACE",)
+    @classmethod
+    def RETURN_TYPES(cls):
+        return ("INSIGHTFACE",)
+
     FUNCTION = "analyze"
     CATEGORY = "face"
 
     def analyze(self, image):
-        # 모델 준비
         if self.app is None:
             self.app = FaceAnalysis(name="buffalo_l")
             self.app.prepare(ctx_id=0)
 
-        # 이미지 전처리
         if isinstance(image, Image.Image):
             img = np.array(image.convert("RGB"))
         else:
             img = image
 
-        # 얼굴 분석 수행
         faces = self.app.get(img)
 
-        # embedding 추출
         embeddings = [
             face.embedding.tolist()
             for face in faces if face.embedding is not None
@@ -52,4 +47,3 @@ NODE_CLASS_MAPPINGS = {
 NODE_DISPLAY_NAME_MAPPINGS = {
     "CustomInsightface": "InsightFace 모델 분석기"
 }
-
